06d9b4f50bab8a83912fbe1bb17b1e215083c91e
djl <dujianglin17@mails.ucas.ac.cn>
2022-05-18 01:05:43 +0800

    simple push

---
+++ cpp/explicit.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+class Point {
+public:
+    int x, y;
+    Point(int x = 0, int y = 0)
+        : x(x), y(y) {}
+};
+
+void displayPoint(const Point& p) 
+{
+    cout << "(" << p.x << "," 
+         << p.y << ")" << endl;
+}
+
+int main()
+{
+    displayPoint(1);
+    Point p = 2;
+    displayPoint(p);
+}

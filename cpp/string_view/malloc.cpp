54e1b30860a53185ee7484567b0c530ff07d5336
杜江霖 <dujianglin17@mails.ucas.ac.cn>
2022-09-16 14:47:24 +0800

    mem copy in string func test

--- cpp/string_view/malloc.cpp
+++ cpp/string_view/malloc.cpp
@@ -7,10 +7,20 @@ void* operator new(size_t t) {
     return malloc(t);
 }
  
-void cpp_api(const std::string&) {}
+void cpp_api(std::string& str) {
+    // char temp[50]  = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+    // assign只需要进行一次拷贝
+    // str.assign(temp, 50);
+    // 下面的写法需要进行两次拷贝
+    // str = string(temp, 50);
+    
+    // string对std::move的构造函数没有特别的优化，依旧要进行一次拷贝
+    // string temp = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+    // string tempp(std::move(temp));
+}
  
 int main() {
-    string s{"hello, world"};
-    cpp_api(s.substr(0, 5));
+    string s("hello, world.hello, world.hello, world.");
+    cpp_api(s);
     return 0;
 }
